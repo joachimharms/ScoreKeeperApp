6c94a65902459af9551b975f53acf3859c2e4dff
joachimharms <joachimharms77@gmail.com>
2017-08-20 21:26:28 +0200

    Implement business logic - done

--- app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.android.scorekeeperapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    public int goalScoreTeam1 = 0;
+    public int goalScoreTeam2 = 0;
+    public int foulScoreTeam1 = 0;
+    public int foulScoreTeam2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -12,4 +18,74 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void increaseGoalTeam1(View v){
+        goalScoreTeam1 += 1;
+        displayGoalScoreTeam1(goalScoreTeam1);
+    }
+
+    public void decreaseGoalTeam1(View v){
+        goalScoreTeam1 -= 1;
+        displayGoalScoreTeam1(goalScoreTeam1);
+    }
+
+    public void increaseGoalTeam2(View v){
+        goalScoreTeam2 += 1;
+        displayGoalScoreTeam2(goalScoreTeam2);
+    }
+
+    public void decreaseGoalTeam2(View v){
+        goalScoreTeam2 -= 1;
+        displayGoalScoreTeam2(goalScoreTeam2);
+    }
+
+
+
+
+
+    public void increaseFoulTeam1(View v){
+        foulScoreTeam1 += 1;
+        displayFoulScoreTeam1(foulScoreTeam1);
+    }
+
+    public void decreaseFoulTeam1(View v){
+        foulScoreTeam1 -= 1;
+        displayFoulScoreTeam1(foulScoreTeam1);
+    }
+
+    public void increaseFoulTeam2(View v){
+        foulScoreTeam2 += 1;
+        displayFoulScoreTeam2(foulScoreTeam2);
+    }
+
+    public void decreaseFoulTeam2(View v){
+        foulScoreTeam2 -= 1;
+        displayFoulScoreTeam2(foulScoreTeam2);
+    }
+
+
+    public void displayGoalScoreTeam1(int score)
+    {
+        TextView scoreView = (TextView) findViewById(R.id.score_team1_view);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayGoalScoreTeam2(int score)
+    {
+        TextView scoreView = (TextView) findViewById(R.id.score_team2_view);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayFoulScoreTeam1(int score)
+    {
+        TextView scoreView = (TextView) findViewById(R.id.foul_team1_view);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayFoulScoreTeam2(int score)
+    {
+        TextView scoreView = (TextView) findViewById(R.id.foul_team2_view);
+        scoreView.setText(String.valueOf(score));
+    }
+
 }
